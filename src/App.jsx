4a6bfbb532e62a9e0a5f53bf416b6b751296a441
Africa-t0t0 <jorge.ramirezz@mail.udp.cl>
2024-11-09 16:40:02 -0300

    default view and buttons components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Sidebar from "./Components/Sidebar";
 import ProjectForm from "./Components/ProjectForm";
+import DefaultView from "./Components/DefaultView";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <ProjectForm />
+      {/* <ProjectForm /> */}
+      <DefaultView />
     </main>
   );
 }
--- src/Components/ProjectForm.jsx
+++ src/Components/ProjectForm.jsx
@@ -2,15 +2,21 @@ import InputField from "./InputField"
 
 export default function ProjectForm() {
     return (
-        <menu>
-            <li>
-                <button>Cancel</button>
-            </li>
-            <li>
-                <button>
-                    Save
-                </button>
-            </li>
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li>
+                    <button
+                        className="text-stone-800 hover:text-stone-950"
+                    >Cancel</button>
+                </li>
+                <li>
+                    <button
+                        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                    >
+                        Save
+                    </button>
+                </li>
+            </menu>
             <div>
                 <InputField
                     labelName={"Title"}
@@ -23,6 +29,6 @@ export default function ProjectForm() {
                     labelName={"Due Date"}
                 />
             </div>
-        </menu>
+        </div>
     );
 }
